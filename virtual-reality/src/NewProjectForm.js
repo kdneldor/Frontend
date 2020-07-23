f3d6b467ac85d60cc7026cb1c9d5e1d2dd6ca22e
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2020-07-23 18:35:34 -0500

    Commit for merge

---
+++ virtual-reality/src/NewProjectForm.js
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+
+export default function NewProjectForm(){
+    return (
+        <form>
+            <label></label>
+            <label></label>
+            <label></label>
+            <label></label>
+            <label></label>
+            <label></label>
+        </form>
+    );
+}
