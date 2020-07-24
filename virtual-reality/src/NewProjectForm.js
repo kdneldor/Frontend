6e56650e4a1a9e3133c0631d787dbac16926f363
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2020-07-23 19:10:26 -0500

    working on project form

--- virtual-reality/src/NewProjectForm.js
+++ virtual-reality/src/NewProjectForm.js
@@ -1,16 +1,31 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-
-export default function NewProjectForm(){
-    return (
-        <form>
-            <label></label>
-            <label></label>
-            <label></label>
-            <label></label>
-            <label></label>
-            <label></label>
-        </form>
-    );
+export default function NewProjectForm() {
+  const [formState, setFormState] = useState({
+    projectTitle: "",
+    projectDescription: "",
+    goalAmount: "",
+    amountReceived: "",
+  });
+  return (
+    <form>
+      <label htmlFor="projectTitle">
+        <input type="text" name="projectTitle" value={formState.projectTitle}></input>
+      </label>
+      <label htmlFor="projectDescription">
+        <textarea type="text" name="projectDescription" value={formState.projectDescription}></textarea>
+      </label>
+      <label htmlFor="goalAmount">
+        <input type="text" name="goalAmount"></input>
+      </label>
+      <label htmlFor="amountReceived">
+        <input type="text" name="amountReceived"></input>
+      </label>
+      <label htmlFor="fundingCompleted">
+        <input type="text" name="fundingCompleted"></input>
+      </label>
+      <label></label>
+    </form>
+  );
 }
